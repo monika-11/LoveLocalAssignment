e63af798e039a24e690280588c8af8c1e4c624d0
monika-11 <100463235+monika-11@users.noreply.github.com>
2023-12-05 11:39:34 +0530

    Add files via upload

---
+++ CountDigitOne.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class CountDigitOne {
+        public int countDigitOne(int n) {
+            int count = 0;
+    
+            for (int i = 1; i <= n; i++) {
+                count += countOnesInNumber(i);
+            }
+    
+            return count;
+        }
+    
+        private int countOnesInNumber(int num) {
+            int count = 0;
+    
+            while (num > 0) {
+                if (num % 10 == 1) {
+                    count++;
+                }
+                num /= 10;
+            }
+    
+            return count;
+        }
+    
+        public static void main(String[] args) {
+            CountDigitOne countDigitOne = new CountDigitOne();
+            System.out.println("enter the number n:");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            
+            int result = countDigitOne.countDigitOne(n);
+    
+            System.out.println("Total number of digit 1 from 1 to " + n + ": " + result);
+        }
+    }
+    
+
