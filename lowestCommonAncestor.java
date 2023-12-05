a0c98732abf00e079312bd2a76f7ed7d55d1f9ae
monika-11 <100463235+monika-11@users.noreply.github.com>
2023-12-05 17:28:30 +0530

    Update lowestCommonAncestor.java

--- lowestCommonAncestor.java
+++ lowestCommonAncestor.java
@@ -7,19 +7,22 @@
  //        the conditions required to meet it they are like
 //             a)leftsubtree<root node
 //              b)rightsubtree>rootnode
- //             c)right and left subtrees are also bst with no duplicates       
-//       so i have created a static class TreeNde which declares variables for root node, left child and right child.
-//       2)Then i have created a constructor to initalizes the node with a given value and setting both left and right child references to null.
-//       3) here i have created a function lowestCommonAncestor which takes root node and 2 nodes as parameters to check LCA of given 2 nodes.
-//       4)In function: i have declared a base class as if root is null i will return null .
-//       5) i have used 2 if statements;
+ //             c)right and left subtrees are also bst with no duplicates 
+            The ancestors are the nodes which exists in a path to reach certain node.hence,
+            lowestcommon ancestor is bascically the first ancestor which is common to both the given nodes.
+          So i understood i need to find lowest means the first ancestor common to both nodes.
+//       2)firsti have created a static class TreeNde which declares variables for root node, left child and right child.
+//       3)Then i have created a constructor to initalizes the node with a given value and setting both left and right child references to null.
+//       4) here i have created a function lowestCommonAncestor which takes root node and 2 nodes as parameters to check LCA of given 2 nodes.
+//       5)In function: i have declared a base class as if root is null i will return null .
+//       6) i have used 2 if statements;
 //           If both nodes  are smaller than the current root value, it means the LCA must be in the left subtree.
 //            Recursively calls lowestCommonAncestor on the left subtree.
 //           If both nodes are greater than the current root value, it means the LCA must be in the right subtree. 
 //                 Recursively calls lowestCommonAncestor on the right subtree.
-//       6) If one node is on the left and the other is on the right, or one of the nodes matches the current root, 
+//       7) If one node is on the left and the other is on the right, or one of the nodes matches the current root, 
 //                 then the current root is the LCA. Returns the current root.
-//       7)Then i have constructed binarysearchtree in main method and called the lca function.
+//       8)Then i have constructed binarysearchtree in main method and called the lca function.
           
       
       
