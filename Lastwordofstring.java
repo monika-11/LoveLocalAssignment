0588e8b24e606b9e24b70128b4e2618f06f833c1
monika-11 <100463235+monika-11@users.noreply.github.com>
2023-12-05 11:37:26 +0530

    Add files via upload

---
+++ Lastwordofstring.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Lastwordofstring {
+
+    public static int lastWordOfString(String s){
+        String s1= s.trim();
+        int count =0;
+        for(int i =s1.length()-1;i>=0;i--){
+            if(s1.charAt(i) != ' '){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        System.out.println("enter the string:");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(lastWordOfString(s));
+    }
+    
+}
