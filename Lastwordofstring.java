8f0a44e5c38f66d3b38d459dd9c5c0bc1c593252
monika-11 <100463235+monika-11@users.noreply.github.com>
2023-12-05 17:11:16 +0530

    Update Lastwordofstring.java

--- Lastwordofstring.java
+++ Lastwordofstring.java
@@ -1,12 +1,14 @@
 // //Easy 1
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 // Explanation:
-// In this program i have using methods of strings in java
-//     here i have created a function called lastWordOfString which takes string as parameter.
-//     here i have used inbuilt function in java which is trim() this function basically removes all the whitespaces present at end and start of  the string.
-//     then i run a loop from end of string(because i need to find count of the lastword ) until start of string and check charcter at index of string whether 
-//     it is notequal to whitespaces if condition satisfied the count is incremented.
-//     else when whitespaces is encountered then we break and come out of loop returning the count of the word
+// The algorithm or approach used in this code is a simple iterative approach 
+//    In this program i have using methods of strings in java
+//      1)here i have created a function called lastWordOfString which takes string as parameter.
+//      2)here i have used inbuilt function in java which is trim() this function basically removes all the whitespaces 
+           present at end and start of  the string.
+//      3)then i run a loop from end of string(because i need to find count of the lastword ) until start of string and
+         check charcter at index of string whether it is notequal to whitespaces if condition satisfied the count is incremented.
+//       else when whitespaces is encountered then we break and come out of loop returning the count of the word
 
 
 
