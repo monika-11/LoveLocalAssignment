4ad92569648750e7808861d9beebb73319f7097d
monika-11 <100463235+monika-11@users.noreply.github.com>
2023-12-05 17:08:14 +0530

    Update lowestCommonAncestor.java

--- lowestCommonAncestor.java
+++ lowestCommonAncestor.java
@@ -1,5 +1,26 @@
-//Medium 1 
-Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+// Medium 1 
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+// Explanation:
+// In this program
+//       The algorithm or approach used in this code is a recursive approach
+//       1)Since a binary Search tree is made of root node ,left child and right child
+//       so i have created a static class TreeNde which declares variables for root node, left child and right child.
+//       2)Then i have created a constructor to initalizes the node with a given value and setting both left and right child references to null.
+//       3) here i have created a function lowestCommonAncestor which takes root node and 2 nodes as parameters to check LCA of given 2 nodes.
+//       4)In function: i have declared a base class as if root is null i will return null .
+//       5) i have used 2 if statements;
+//           If both nodes  are smaller than the current root value, it means the LCA must be in the left subtree.
+//            Recursively calls lowestCommonAncestor on the left subtree.
+//           If both nodes are greater than the current root value, it means the LCA must be in the right subtree. 
+//                 Recursively calls lowestCommonAncestor on the right subtree.
+//       6) If one node is on the left and the other is on the right, or one of the nodes matches the current root, 
+//                 then the current root is the LCA. Returns the current root.
+//       7)Then i have constructed binarysearchtree in main method and called the lca function.
+          
+      
+      
+      
+      
       
 import java.util.*;
 public class lowestCommonAncestor{
